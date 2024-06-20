03b125ccf6b99dc41b9d455142ce39cc6fa1eecc
Hiarlizindograu <hiarleycostamarley@gmail.com>
2024-06-20 13:16:43 -0300

    JAVA SCRIPT Q FUNCIONA2

--- js/doc.js
+++ js/doc.js
@@ -1,18 +1,21 @@
-// const dsd = document.getElementById('imagens-ocultas');
 
-// botao.addEventListener('click', mostrarEsconderImagem);
-// function mostrarEsconderImagem() {
-//     if (imagens-ocultas.style.display === 'none') {
-//       minhaImagem.style.display = 'block';
-//     } else {
-//       minhaImagem.style.display = 'none';
-//     }
-//   }
 document.getElementById('botaojs').addEventListener('click', function() {
   var element = document.getElementById('imagens-ocultas');
   if (element.style.display === 'none') {
       element.style.display = 'block';
   } else {
       element.style.display = 'none';
+  }
+});
+
+document.getElementById('botaojs').addEventListener('click', function() {
+  var element = document.getElementById('imagens-ocultas');
+  var textElement = document.getElementById('vermais');
+  if (element.classList.contains('hidden')) {
+      element.classList.remove('hidden');
+      textElement.textContent = "VER MENOS";
+  } else {
+      element.classList.add('hidden');
+      textElement.textContent = "VER MAIS";
   }
 });
