13aaab7372faad569d0fd8ae7d9bd941e7b2ba4c
Hiarlizindograu <hiarleycostamarley@gmail.com>
2024-06-20 12:26:26 -0300

    JAVA SCRIPT

--- js/doc.js
+++ js/doc.js
@@ -1,1 +1,18 @@
-// test
+// const dsd = document.getElementById('imagens-ocultas');
+
+// botao.addEventListener('click', mostrarEsconderImagem);
+// function mostrarEsconderImagem() {
+//     if (imagens-ocultas.style.display === 'none') {
+//       minhaImagem.style.display = 'block';
+//     } else {
+//       minhaImagem.style.display = 'none';
+//     }
+//   }
+document.getElementById('botaojs').addEventListener('click', function() {
+  var element = document.getElementById('imagens-ocultas');
+  if (element.style.display === 'none') {
+      element.style.display = 'block';
+  } else {
+      element.style.display = 'none';
+  }
+});
